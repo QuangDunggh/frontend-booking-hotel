d2987561861889bd0d54f581f33f0c6c4494918d
Quang Dung <dphanquang@gmail.com>
2024-01-18 08:23:29 +0700

    update

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => {
+  let day = new Date();
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <Row>
+          <Col xs={12} md={12} className='text-center'>
+              <p>&copy; {day.getFullYear()} lakeSide Hotel</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav navbar-expand-lg bg-body-tertirary px-5 shadow mt-5 sticky-top'>
+      <div className='container-fluid'>
+        <Link to={"/"}>
+          <span className='hotel-color'>LakeSide Hotel</span>
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls='navbarScroll'
+          aria-expand="false"
+          aria-label="Toggle navigation"
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id="navbarScroll">
+          <ul className='navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll'>
+            <li className='nav-item'>
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
